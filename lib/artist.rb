00b38b671e739da11025420940c1324778fe7edd
Mary-Kate James <marykatj@gmail.com>
2018-06-08 01:31:07 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+
+attr_accessor :name
+
+def initialize
+  @name = name
+end
+
+def name
+  @name
+end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+
+attr_accessor :title, :author
+
+def initialize
+  @title = title
+end
+
+def title
+  @title
+end
+
+end
